f2684b5d4937e1b7e7285f979e195e30832c695b
LanorTrad <140711077+L4NOR@users.noreply.github.com>
2024-09-27 19:11:52 +0200

    Four commit

---
+++ satsudou/js/intro.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById('intro').style.display = 'none';
+        document.getElementById('main-content').style.opacity = '1';
+    }, 3000);
+});
