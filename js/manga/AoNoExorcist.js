f47b0945431c787454018c428e11122311b6b1cd
LANOR <ytlanor@gmail.com>
2025-06-08 13:19:35 +0200

    '158'

--- js/manga/AoNoExorcist.js
+++ js/manga/AoNoExorcist.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    maxChapters: 157,
+    maxChapters: 158,
     currentManga: "Ao No Exorcist",
     chapterPrefix: "Chapitre"
 };
--- js/utile/genres.js
+++ js/utile/genres.js
@@ -5,7 +5,7 @@ const mangas = [
         title: "Ao No Exorcist",
         genres: ["Action", "Aventure", "Fantasy", "LanorTrad"],
         status: "En cours",
-        chapters: 157,
+        chapters: 158,
         description: "Rin Okumura est un adolescent qui découvre un jour qu'il est le fils de Satan. Déterminé à devenir un exorciste pour vaincre Satan...",
         image: "images/cover/AoNoExorcist.jpg"
     },
