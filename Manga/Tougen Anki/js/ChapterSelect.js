f8a6b3b58190b29d92758eb46c57994a338cda38
LanorTrad <orlanferrier910@gmail.com>
2025-09-01 17:12:57 +0200

    205

--- Manga/Tougen Anki/js/ChapterSelect.js
+++ Manga/Tougen Anki/js/ChapterSelect.js
@@ -1,4 +1,4 @@
-const totalChapters = 204; // Nombre total de chapitres
+const totalChapters = 205; // Nombre total de chapitres
 
 // Liste complète des chapitres, avec bonus
 const chaptersList = [
--- js/manga/Tougen.js
+++ js/manga/Tougen.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    maxChapters: 204,
+    maxChapters: 205,
     currentManga: "Tougen Anki",
     chapterPrefix: "Chapitre",
     baseDate: new Date(2025, 0, 1)
