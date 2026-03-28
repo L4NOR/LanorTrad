aa9312dbfc0a1897e237c30ae92c7c2d7e9b4798
LanorTrad <140711077+L4NOR@users.noreply.github.com>
2026-03-28 01:25:36 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,22 +1,2 @@
-// v3.0 - LanorTrad Service Worker (OneSignal + notifications)
-
-// Importer le SDK OneSignal pour les push notifications
+// v3.1 - LanorTrad Service Worker (OneSignal push notifications)
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-// Handler : clic sur une notification custom (hors OneSignal)
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
-            for (var i = 0; i < clientList.length; i++) {
-                var client = clientList[i];
-                if (client.url && client.url.indexOf('lanortrad') !== -1 && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
-        })
-    );
-});
